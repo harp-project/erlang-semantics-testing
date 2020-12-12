c07c762cf8e8b7ddda39851939d905a2f6886715
daniel-horpacsi <daniel-h@elte.hu>
2020-12-12 12:30:44 +0100

    support for multiple random tests with eqc:numtests + minor refactorings

--- scripts.erl
+++ scripts.erl
@@ -11,7 +11,7 @@
 -define(REPORT_DIRECTORY, "./reports/").
 -define(COQ_FILENAME, "./reports/coq_coverage.csv").
 
--define(ENABLE_SHRINKING, true).
+-define(ENABLE_SHRINKING, false).
 
 remove_extension(Filename) ->
     hd(string:split(Filename, ".", trailing)).
@@ -136,61 +136,20 @@ write_to_file(Filename, Content, Mode) ->
             io:format("Error opening file ~s: ~s", [Status, Msg])
     end.
 
-generator_remove_junk(Input) ->
-    hd(string:split(Input, "----------", trailing)).
-
-generate_and_save_random_test(Id, ReportDirectory) ->
-    random:seed(erlang:now()),
-    ModuleName = io_lib:format("module~p", [Id]),
-    Basename = ModuleName ++ ".erl",
-    Filename = ReportDirectory ++ Basename,
-    % TODO: egg_tester:y() should return with a string instead of printing it
-    %       write_to_file(Filename, io_lib:format('-module(module~p).~n-export([main/0]).~n~p', [Id, egg_tester:y()]), write),
-    case
-        exec:shell_exec(
-            io_lib:format("erl -pa eqc-2.01.0/ebin -pa generator/ebin -noshell -eval \"random:seed(erlang:now()), io:format('~~p', [erl_2020_gen:sample(module~p, ~p)])\" -eval 'init:stop()'",
-                          [Id, 10])
-        )
-    of
-        {0, Output} ->
-            write_to_file(
-                Filename,
-                generator_remove_junk(Output),
-                write
-            );
-        {_, Output} ->
-            io:format("Cannot generate code ~p~n", [Output])
-    end,
-    %NOTE: this is kinda dirty, but some generated erlang code either won't compile or
-    %      crashes durring execution due to ill formed code.
-    case execute_erl:execute(Filename, ModuleName, ReportDirectory) of
-       {error, _} -> generate_and_save_random_test(Id, ReportDirectory);
-       _          -> Filename
-    end.
-
 count_if(Pred, Lists) ->
     length(lists:filter(Pred, Lists)).
 
 count(Elem, Lists) ->
     count_if(fun(X) -> Elem == X end, Lists).
 
-run_multiple_test(Tests) when is_list(Tests) ->
+unit_test(Tests) when is_list(Tests) ->
     ReportDirectory = mktmpdir(),
-    lists:map(fun(Test) -> execute_and_compare_result(Test, ReportDirectory) end, Tests).
-
-generate_and_multiple_test(NumberOfTests) when is_number(NumberOfTests) ->
-    ReportDirectory = mktmpdir(),
-    lists:map(
-        fun(Id) ->
-            Test = generate_and_save_random_test(Id, ReportDirectory),
-            Result = execute_and_compare_result(Test, ReportDirectory),
-            Result
-        end,
-        lists:seq(1, NumberOfTests)
-    ).
+    [execute_and_compare_result(Test, ReportDirectory) || Test <- Tests].
 
 %% ---------------------------------------------------------------------
-%% Proper integration to QC
+%% QuickCheck
+
+-define(ModuleNamePlaceHolder, "modulenameplaceholder").
 
 save_test(String, Id, ReportDirectory) ->
     ModuleName = io_lib:format("module~p", [Id]),
@@ -205,41 +164,39 @@ is_compilable(T) ->
     element(1, C) /= ok andalso io:format("\nnem fordul:(\n"),
     element(1, C) == ok.
 
-generate_and_test_qc() ->
+random_test(NumTests) ->
+    put(test_id, 0),
     random:seed(erlang:now()),
     ReportDirectory = mktmpdir(),
-    ModuleName = "module1", %TODO
-    
-    G = resize(20, erlgen:module(ModuleName, 20)),
+    G = resize(20, erlgen:module(?ModuleNamePlaceHolder, 20)),
     G2 = ?LET(M, G, case lists:keysearch(value, 1, M) of
                         {value, {_, Value}} -> Value;
                         false -> []
                     end),
     G3 = ?SUCHTHAT(T, G2, is_compilable(T)),
-    P = ?FORALL(T, if ?ENABLE_SHRINKING -> G3; true -> noshrink(G3) end, ?WHENFAIL(io:format("~n~s~n", [erl_prettypr:format(eqc_symbolic:eval(T))]),
-        begin
-            ProgramText = erl_prettypr:format(eqc_symbolic:eval(T)),
-            FilePath = ReportDirectory ++ ModuleName ++ ".erl",
-            write_to_file(FilePath, ProgramText, write),
-            execute_and_compare_result(FilePath, ReportDirectory)
-        end)),
-    [eqc:quickcheck(eqc:numtests(1, P))].
+    P = ?FORALL(T, if ?ENABLE_SHRINKING -> G3; true -> noshrink(G3) end,
+          %?WHENFAIL(io:format("~n~s~n", [erl_prettypr:format(eqc_symbolic:eval(T))]),
+            begin
+                TestId = get(test_id),
+                ModuleName = io_lib:format("module~p", [TestId]),
+                put(test_id, TestId+1),
+                RawProgramText = erl_prettypr:format(eqc_symbolic:eval(T)),
+                ProgramText = re:replace(RawProgramText, ?ModuleNamePlaceHolder, ModuleName, [global, {return, list}]),
+                FilePath = ReportDirectory ++ ModuleName ++ ".erl",
+                write_to_file(FilePath, ProgramText, write),
+                execute_and_compare_result(FilePath, ReportDirectory)
+            end
+          %)
+        ),
+    [eqc:quickcheck(eqc:numtests(NumTests, P))],
+    noreport.
 
 %% ---------------------------------------------------------------------
 
-parser_main_arguments([]) ->
-    help;
-parser_main_arguments(Args) when is_list(Args) ->
-    case hd(Args) of
-        "random" -> {runRandomTests, list_to_integer(lists:nth(2, Args))};
-        "randomqc" -> runRandomTestQC;
-        _ -> {runTests, Args}
-    end;
-parser_main_arguments(_) ->
-    help.
-
 report_result(help) ->
     io:format("wrong number of arguments~n");
+report_result(noreport) ->
+    noreport;
 report_result(Results) ->
     io:format("All ~p Passed ~p Failed ~p~n", [
         length(Results),
@@ -290,15 +247,23 @@ pp_map(Map) when is_map(Map) ->
   %% workaround, we need foreach
   maps:fold(fun(K, V, Acc) -> io:format("~p rule was used ~p times~n", [K, V]), Acc end, 0, Map).
 
+parser_main_arguments([]) ->
+    help;
+parser_main_arguments(Args) when is_list(Args) ->
+    case Args of
+        ["random", N] -> {runRandTests, list_to_integer(N)};
+        _             -> {runUnitTests, Args}
+    end;
+parser_main_arguments(_) ->
+    help.
+
 main(Args) ->
   %% Initialize with the Coq coverage map, where all rules were used 0 times:
     put(coq_coverage_map, default_map()),
-    
-    case parser_main_arguments(Args) of
-        {runRandomTests, NoT} -> Results = generate_and_multiple_test(NoT);
-        runRandomTestQC -> Results = generate_and_test_qc();
-        {runTests, Tests} -> Results = run_multiple_test(Tests);
-        help -> Results = help;
-        true -> Results = help
+
+    Results = case parser_main_arguments(Args) of
+        {runRandTests, NoT} -> random_test(NoT);
+        {runUnitTests, LoT} -> unit_test(LoT);
+        _                   -> help
     end,
     report_result(Results).
