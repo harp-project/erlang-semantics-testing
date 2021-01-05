013145f458d6ed18f1694185b5b82dcfc8b7f3f1
berpeti <peti.ber007@gmail.com>
2021-01-05 17:19:55 +0100

    Add counterexample

---
+++ shrunk codes/module15_correct.erl
@@ -0,0 +1,136 @@
+-module(module45).
+
+-export([main/0]).
+
+main() ->
+    A = case [] -- [0 div length([])] of
+	  [] -> 0;
+	  _ -> false
+	end,
+    0.
+
+
+%original
+
+-module(module0).
+
+-export([main/0]).
+
+main() ->
+    FX = case tuple_to_list(list_to_tuple([] ++ [])) --
+		[length([]), [0, 6, 20, {}] -- [true, 0, true],
+		 {[], harbour, length([])}, length([]) div length([]),
+		 []]
+	     of
+	   [false, false, false, true, false, true] -> 0;
+	   [false, false] ->
+	       length(tuple_to_list({{}, [], false, 'WATER'}) --
+			[LQEN
+			 || WW <- [], LQEN <- [], QoI <- [], GLyjM <- [],
+			    false]);
+	   _ ->
+	       not (not (not false) and (not true > (true == true)))
+	 end,
+    MX = [list_to_tuple(tuple_to_list(list_to_tuple([[],
+						     {}]))),
+	  0 * 0 * 0 * (0 * 5 - 0) + length(tuple_to_list({}))],
+    A = length([not (0 == [])
+		|| DoEshVy <- tuple_to_list({}), MX -- [] >= {},
+		   question /= list_to_tuple([]), 'THIN' == letter]),
+    NzFvKtoi = not ('DEAD' =< 0),
+    case NzFvKtoi of
+      secret -> {};
+      secret -> 8;
+      secret -> 0 + (0 - 17 - 0 rem 18) - 6;
+      secret -> length([[], 'FRIEND' >= {[]}, NzFvKtoi]);
+      _ ->
+	  case 'GOOD' of
+	    -17 -> false;
+	    _ -> length(tuple_to_list({MX, [], 0, NzFvKtoi, 13}))
+	  end
+    end.
+
+person(Pzyl) ->
+    tomorrow({fat}),
+    BqrcA = pipe,
+    XqBiw = length([case case list_to_tuple([]) of
+			   {42, 42, 42, 42, 42, 42, 42, 42, 42, 42, 42, 42, 42,
+			    42, 42, 42} ->
+			       tuple_to_list(Pzyl);
+			   {42, 42, 42, 42, 42, 42, 42, 42, 42, 42, 42, 42, 42,
+			    42, 42, 42} ->
+			       2;
+			   _ -> []
+			 end
+			of
+		      [[], [], [42, 42], [42, 42, 42, 42], [42]] -> BqrcA;
+		      _ -> ([] == []) and (false and true)
+		    end,
+		    case case case {} of
+				{} -> Pzyl;
+				_ -> {}
+			      end
+			     of
+			   {42, 42, 42, 42, 42, 42, 42, 42} -> 12;
+			   {42, 42, 42, 42, 42, 42, 42, 42} ->
+			       {BqrcA, place, BqrcA, true, BqrcA};
+			   {42, 42, 42, 42, 42, 42, 42, 42} -> [];
+			   _ -> list_to_tuple([])
+			 end
+			of
+		      {} -> Pzyl;
+		      {} -> card;
+		      {} -> Pzyl;
+		      {} -> list_to_tuple(tuple_to_list({}));
+		      {} -> BqrcA;
+		      _ -> 14 rem 3 - 0
+		    end,
+		    case [] of
+		      [] -> 13 + 0 + length([]);
+		      [] -> [] ++ tuple_to_list(Pzyl);
+		      [] -> list_to_tuple([]);
+		      [] -> not (not false);
+		      _ ->
+			  case case [] of
+				 [{-12, [115, 109, 121, 119, 106], 17, 12,
+				   true},
+				  {8, [122, 122, 122, 105, 111, 117], 17, 16,
+				   true},
+				  {-20, [122, 113, 99, 121, 111, 111], 17, 8,
+				   true},
+				  {-9, [116, 100, 120, 98], 17, -1, false},
+				  {-19, [120, 119, 108, 121, 122, 97, 98], 17,
+				   12, true},
+				  {-3, [110, 102, 108, 104, 102, 112], 17, 8,
+				   false}] ->
+				     'BOAT';
+				 [{-5, [122, 102, 104, 114, 99, 100, 100], 17,
+				   16, false},
+				  {3, [112], 17, 16, false},
+				  {6, [108, 110, 105, 122, 101, 120, 100, 120],
+				   17, -1, false},
+				  {-13, [122, 120, 118, 121, 122, 101, 102], 17,
+				   8, false},
+				  {3, [104, 98, 109, 107, 114, 110, 106, 110],
+				   17, -1, false},
+				  {16, [110, 104, 106, 105, 101], 17, 12,
+				   false}] ->
+				     BqrcA;
+				 _ -> BqrcA
+			       end
+			      of
+			    business -> start;
+			    business -> {love, BqrcA, []};
+			    business -> 1;
+			    _ -> 9
+			  end
+		    end]),
+    not true.
+
+tomorrow(L) ->
+    person({}),
+    Pw = {},
+    NKKKyyPf = ((0 - 0 /= 0) or false) and
+		 (not (true or false) or false)
+		 or (girl < [list_to_tuple([]), (0 - 0) div (5 * 13)]),
+    yesterday.
